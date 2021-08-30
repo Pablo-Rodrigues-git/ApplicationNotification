3a395ed8b0043cc4e7c672203e3fb915b5cdd14b
Pablo-Rodrigues-git <pablo.rodrigues2015@outlook.com>
2021-08-30 02:06:34 -0300

    Installed 2 libraries from retrofit and 2 libraries from Glide

--- app/src/main/java/com/courses/applicationnotification/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/courses/applicationnotification/MyFirebaseMessagingService.kt
@@ -6,6 +6,8 @@ import androidx.annotation.RequiresApi
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
+
+
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
   private val tag = "FirebaseMessagingServ"
@@ -14,9 +16,9 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         Log.i(tag, remoteMessage.from.toString())
 
         if(remoteMessage.notification != null){
-            this.showNotification("1234",
-            remoteMessage.notification?.title.toString(),
-            remoteMessage.notification?.body.toString())
+            this.showNotification("1234", "Bootcamp Android", "Curso Kotlin Android")
+            remoteMessage.notification?.title.toString()
+            remoteMessage.notification?.body.toString()
         }
     }
 }
