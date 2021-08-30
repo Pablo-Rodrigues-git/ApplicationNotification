d82d1ad181ebd19c3f12c383480958a3aa43f350
Pablo-Rodrigues-git <pablo.rodrigues2015@outlook.com>
2021-08-30 01:39:45 -0300

    Created MyFirebaseMessagingService.kt file

---
+++ app/src/main/java/com/courses/applicationnotification/MyFirebaseMessagingService.kt
@@ -0,0 +1,22 @@
+package com.courses.applicationnotification
+
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+  private val tag = "FirebaseMessagingServ"
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.i(tag, remoteMessage.from.toString())
+
+        if(remoteMessage.notification != null){
+            this.showNotification("1234",
+            remoteMessage.notification?.title.toString(),
+            remoteMessage.notification?.body.toString())
+        }
+    }
+}
