572976e0fc0e144ef039bc88c60234cd3350c2f7
Pablo-Rodrigues-git <pablo.rodrigues2015@outlook.com>
2021-08-30 01:39:30 -0300

    Created FireBaseMessaging instance

--- app/src/main/java/com/courses/applicationnotification/MainActivity.kt
+++ app/src/main/java/com/courses/applicationnotification/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.courses.applicationnotification
 
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.FirebaseApp.getInstance
+import com.google.firebase.messaging.FirebaseMessaging
+import java.util.Calendar.getInstance
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    @SuppressLint("StringFormatInvalid")
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +31,21 @@ class MainActivity : AppCompatActivity() {
             this.showNotification("1234", "Bootcamp Android", "Curso Kotlin Android ")
 
         }
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = getString(R.string.msg_token_fmt, token)
+            Log.d(TAG, msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
 
     }
 }
